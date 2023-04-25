b3e299c9d0bcbba3e481aa31798f968ad011a677
Jennifer <jennifer@jmeara.com>
2023-04-25 08:57:25 -0400

    initial commit

---
+++ project-1/books.py
@@ -0,0 +1,23 @@
+# Project 1 FastAPI books server
+# to run `uvicorn books:app --reload`
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+# async is not needed with FastAPI b/c it does it behind the scenes
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+# to run `uvicorn books:app --reload`
+
